0710bbfb4c839c3e5d10ddf9f3f1097e9cf4d9d0
jhsware <sebastian@urbantalk.se>
2021-09-24 19:32:25 +0200

    Fix tests in templates

--- packages/cia-template-typescript/template/src/App.test.tsx
+++ packages/cia-template-typescript/template/src/App.test.tsx
@@ -1,9 +1,12 @@
-import Inferno from 'inferno';
-import { render, screen } from '@testing-library/inferno';
+import { render } from 'inferno';
 import App from './App';
 
+let container = document.createElement('div');
+
 test('renders learn inferno link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn inferno/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />, container);
+  // const linkElement = screen.getByText(/learn inferno/i);
+  expect(
+    container.getElementsByClassName('App-link').length
+  ).toBe(1);
 });
--- packages/cia-template/template/src/App.test.js
+++ packages/cia-template/template/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/inferno';
+import { render } from 'inferno'
+
 import App from './App';
 
+let container = document.createElement('div');
+
 test('renders learn inferno link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn inferno/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />, container);
+
+  // const linkElement = screen.getByText(/learn inferno/i);
+  expect(
+    container.getElementsByClassName('App-link').length
+  ).toBe(1);
 });
