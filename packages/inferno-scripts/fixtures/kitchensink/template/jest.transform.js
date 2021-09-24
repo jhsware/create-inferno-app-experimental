bea37553b29b574450278c4b85ea2097236720dc
jhsware <sebastian@urbantalk.se>
2021-09-24 23:04:06 +0200

    Applied PR https://github.com/facebook/create-react-app/pull/11338

--- packages/inferno-scripts/fixtures/kitchensink/template/jest.transform.js
+++ packages/inferno-scripts/fixtures/kitchensink/template/jest.transform.js
@@ -1,3 +1,5 @@
 const babelOptions = { presets: ['inferno-app'] };
 
-module.exports = require('babel-jest').createTransformer(babelOptions);
+const babelJest = require('babel-jest').default;
+
+module.exports = babelJest.createTransformer(babelOptions);
