8a95d60ec52f4cfd2ccce2c607848c5059402b9b
jhsware <sebastian@urbantalk.se>
2021-09-25 09:16:47 +0200

    Fix error "Inferno must be in scope for JSX"

--- packages/cia-template/template/src/App.js
+++ packages/cia-template/template/src/App.js
@@ -1,3 +1,4 @@
+import Inferno from 'inferno';
 import logo from './logo.svg';
 import './App.css';
 
--- packages/cia-template/template/src/index.js
+++ packages/cia-template/template/src/index.js
@@ -1,4 +1,4 @@
-import { render } from 'inferno';
+import Inferno, { render } from 'inferno';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
